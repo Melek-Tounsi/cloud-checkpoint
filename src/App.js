71ab7e27e17f9eb66cb3e97cd49cf4e070d96a56
Melek-Tounsi <melektt99@gmail.com>
2023-05-10 18:46:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12770.177183739364!2d10.248945869559172!3d36.85338852563573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2caa8ffcdd42b%3A0x370637670504b1a5!2sEl%20Aouina%2C%20Tunis!5e0!3m2!1sfr!2stn!4v1683727052023!5m2!1sfr!2stn" 
+      width="600" 
+      height="450" 
+      style={{border:"0"}}
+      allowfullscreen="" 
+      loading="lazy" 
+      referrerpolicy="no-referrer-when-downgrade">
+      </iframe>
     </div>
   );
 }
